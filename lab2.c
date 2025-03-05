0ba7cc990439bafc9f433d2f6c33ba02cb832973
Doru-1 <teodorstere@gmail.com>
2025-03-05 14:45:46 +0200

    Am scos o bucata nefunctionala de cod si am stilizat. To-Do: Nimic

--- lab2.c
+++ lab2.c
@@ -1,17 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
+int function(int x)
+{
+	x = x - 2;
+	// x = function(x); <-------- ce face asta?? ~Doru
 	return x;
 }
 
-int main(){
+int main()
+{
 	int a=30, i=0;
 	
-	while (a>0){
-		a=function(a);
+	while (a > 0)
+	{
+		a = function(a);
 		i++;
 	}
 	printf("%d", i);
